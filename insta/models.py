795815c9fc8a534b23e4b970b6dd0a92d0a7dc29
Benard18 <benkaranja43@gmail.com>
2018-07-30 18:12:56 +0300

    adding the comments class

--- insta/models.py
+++ insta/models.py
@@ -30,4 +30,14 @@ class Image(models.Model):
 	date_uploaded = models.DateTimeField(auto_now=True)
 	likes = models.IntegerField(default=0)
 	caption = models.CharField(max_length=140, default="")
-	tags = models.ManyToManyField(Tags,related_name="tags",blank=True)
+	tags = models.ManyToManyField(Tags,related_name="tags",blank=True)	tags = models.ManyToManyField(Tags,related_name="tags",blank=True)
+
+	@property
+	def get_comments(self):
+		return self.comments.all()
+	
+
+class Comments(models.Model):
+	user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE,related_name='user')
+	comments = models.TextField()
+	date_posted = models.DateTimeField(auto_now=True)
