92636fedd57aa98fc6621b043798fc016b6528b0
Benard18 <benkaranja43@gmail.com>
2018-07-30 18:11:45 +0300

    addin the userprofile classes

--- insta/models.py
+++ insta/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from tinymce.models import HTMLField
+# Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+	status = HTMLField()
+	profilepicture = models.ImageField(upload_to='images/', blank=True,default="/jVr43h8.png")
+	secondary_email = models.CharField(max_length=100, null=True, blank=True)
+
+	@receiver(post_save, sender=User)
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+				UserProfile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User)
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
