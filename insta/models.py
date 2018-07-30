741d1084c0c927aa4d32dd63d6531324e7e75dc6
Benard18 <benkaranja43@gmail.com>
2018-07-30 18:11:19 +0300

    addin ghte necessary imports

--- insta/models.py
+++ insta/models.py
@@ -2,4 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from tinymce.models import HTMLField
