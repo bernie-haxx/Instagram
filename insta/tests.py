ee7918c65e18198343cfef359cbabd35bb3921ad
Benard18 <benkaranja43@gmail.com>
2018-08-01 15:06:50 +0300

    making tests for my models so as to achieve my objective on the rubric of moringa school

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import UserProfile,Tags,Image,Comments
+import forgery_py
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+class tagsTestClass(TestCase):
+	def setUp(self):
+		self.ben= Tags(name=forgery_py.name.first_name())
 
-# Create your tests here.
+	def test_instance(self):
+		self.assertTrue(isinstance(self.ben,Tags))	
+
+	def test_save_method(self):
+		self.ben.save_tags()
+		tags = Tags.objects.all()
+		self.assertTrue(len(tags)>0)
+
+class UserProfileTestClass(TestCase):
+	def test_profile_creation(self):
+		Users = get_user_model()
+		user = Users.objects.create(username="jeff", password="django-tutorial")
+		self.assertIsInstance(user.profile, UserProfile)	
+		user.save()
+		self.assertIsInstance(user.profile, UserProfile)
+		
+
